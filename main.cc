3b8ea246f51c1a14fe1f8ca90e9cb129b3524158
furokl <furokl@mail.ru>
2023-02-10 04:53:04 +0300

    last

--- main.cc
+++ main.cc
@@ -1,7 +1,8 @@
 #include <drogon/drogon.h>
 #include <chrono>
-
+#define USE_POSTGRESQL
 using namespace drogon;
+using namespace drogon::orm;
 using namespace std::chrono_literals;
 
 typedef std::function<void(const HttpResponsePtr &)> Callback;
@@ -23,35 +24,44 @@ void p_logout(const HttpRequestPtr &req, Callback &&callback) {
     callback(resp);
 }
 
-void p_login(const HttpRequestPtr &req, Callback &&callback) {
-    HttpResponsePtr resp = HttpResponse::newHttpResponse();
-    std::string user = req->getParameter("user");
-    std::string passwd = req->getParameter("passwd");
-
-    if (user == "user" && 
-        // TO DO Use Argon2 or BCrypt
-        utils::getMd5("jadsjhdsajkh" + passwd) == "5B5299CF4CEAE2D523315694B82573C9")
+auto p_login(const HttpRequestPtr req) -> Task<HttpResponsePtr> {
+    auto esq = app().getDbClient("furokl_db");
+    try
     {
-        req->session()->insert("loggedIn", true);
-        resp->setBody("<script>window.location.href = \"/\";</script>");
-        callback(resp);
+        std::string user = req->getParameter("user");
+        std::string passwd = req->getParameter("passwd");
+        auto result = co_await esq->execSqlCoro( // execSqlSync
+            "SELECT COUNT(*) FROM \"Account\" "
+            "WHERE "
+            "\"username\" = $1 AND "
+            "\"password\" = $2",
+            user, passwd); 
+        auto resp = HttpResponse::newHttpResponse();
+        if (result[0][0].as<size_t>() > 0)
+        {
+            req->session()->insert("loggedIn", true);
+            resp->setBody("<script>window.location.href = \"/\";</script>");
+            co_return resp;
+        }
+        else 
+        {
+            resp->setStatusCode(k401Unauthorized);
+            resp->setBody("<script>window.location.href = \"/\";</script>");
+            co_return resp;
+        }
     }
-    else
+    catch (const DrogonDbException &e)
     {
-        resp->setStatusCode(k401Unauthorized);
-        resp->setBody("<script>window.location.href = \"/\";</script>");
-        callback(resp);
+        auto resp = HttpResponse::newHttpResponse();
+        resp->setBody(e.base().what());
+        co_return resp;
     }
 }
 
 int main()
 {
-
-    LOG_INFO << "Server started...";
     app()
-        .enableSession(24h)
-        //.loadConfigFile("../config.json")
-        .addListener("192.168.0.105", 80)
+        .loadConfigFile("../config.json")
         .registerHandler("/", &p_default, { Get })
         .registerHandler("/logout", &p_logout, { Post })
         .registerHandler("/login", &p_login, { Post })
